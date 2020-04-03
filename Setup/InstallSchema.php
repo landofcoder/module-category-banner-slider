6697773f15681b4c8c892fc488927ee1973fb7f0
hieu <hieu01412@gmail.com>
2020-04-03 11:06:42 +0700

    push code

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -1,17 +1,17 @@
 <?php
 /**
  * Copyright (c) 2019 landofcoder.com
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -41,138 +41,138 @@ class InstallSchema implements InstallSchemaInterface
     public function install(
         SchemaSetupInterface $setup,
         ModuleContextInterface $context
-    ) {
+    )
+    {
         $installer = $setup;
         $installer->startSetup();
 
         $lof_category_banner_table = $installer->getConnection()->newTable($installer->getTable('lof_category_banner'))
-                ->addColumn(
-                    'banner_id',
-                    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
-                    null,
-                    ['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true],
-                    'Banners ID'
-                )
-                ->addColumn(
-                    'title',
-                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                    255,
-                    ['nullable' => true, 'default' => null],
-                    'Title'
-                )
-                ->addColumn(
-                    'description',
-                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                    255,
-                    [],
-                    'Description'
-                )
-                ->addColumn(
-                    'priority',
-                    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
-                    null,
-                    ['nullable' => false, 'unsigned' => true, 'default' => '0'],
-                    'Priority'
-                )
-                ->addColumn(
-                    'images',
-                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                    "6M",
-                    ['nullable' => true, 'default' => null],
-                    'All album images will store with json data or serialized data into the column'
-                )
-                ->addColumn(
-                    'status',
-                    \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
-                    null,
-                    ['nullable' => false, 'unsigned' => true, 'default' => '1'],
-                    'Date'
-                )
-                ->addColumn(
-                    'created_time',
-                    \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
-                    null,
-                    ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT],
-                    'Creation Time'
-                )
-                ->addColumn(
-                    'update_time',
-                    \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
-                    null,
-                    ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE],
-                    'Update Time'
-                );
-        
+            ->addColumn(
+                'banner_id',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true],
+                'Banners ID'
+            )
+            ->addColumn(
+                'title',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                255,
+                ['nullable' => true, 'default' => null],
+                'Title'
+            )
+            ->addColumn(
+                'description',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                255,
+                [],
+                'Description'
+            )
+            ->addColumn(
+                'priority',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['nullable' => false, 'unsigned' => true, 'default' => '0'],
+                'Priority'
+            )
+            ->addColumn(
+                'images',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                "6M",
+                ['nullable' => true, 'default' => null],
+                'All album images will store with json data or serialized data into the column'
+            )
+            ->addColumn(
+                'status',
+                \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+                null,
+                ['nullable' => false, 'unsigned' => true, 'default' => '1'],
+                'Date'
+            )
+            ->addColumn(
+                'created_time',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+                null,
+                ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT],
+                'Creation Time'
+            )
+            ->addColumn(
+                'update_time',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+                null,
+                ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE],
+                'Update Time'
+            );
+
         $lof_category_banner_store_table = $installer->getConnection()->newTable($installer->getTable('lof_category_banner_store'))
-                ->addColumn(
-                    'banner_id',
-                    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
-                    null,
-                    ['unsigned' => true, 'nullable' => false, 'primary' => true],
-                    'Banner ID'
-                )
-                ->addColumn(
-                    'store_id',
-                    \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
-                    null,
-                    [
-                     'unsigned' => true,
-                     'nullable' => false,
-                     'primary'  => true,
-                    ],
-                    'Store Id'
-                )
-                ->addIndex(
-                    $installer->getIdxName(('lof_category_banner_store', ['banner_id']),
-                    ['banner_id']
-                )->addForeignKey(
-                    $installer->getFkName($'lof_category_banner_store', 'banner_id', 'lof_category_banner', 'banner_id'),
-                    'banner_id',
-                    $installer->getTable('lof_category_banner'),
-                    'banner_id',
-                    \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
-                )->addForeignKey(
-                    $installer->getFkName('lof_category_banner_store', 'store_id', 'store', 'store_id'),
-                    'store_id',
-                    $installer->getTable('store'),
-                    'store_id',
-                    \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
-                )
-                ;
+            ->addColumn(
+                'banner_id',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['unsigned' => true, 'nullable' => false, 'primary' => true],
+                'Banner ID'
+            )
+            ->addColumn(
+                'store_id',
+                \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+                null,
+                [
+                    'unsigned' => true,
+                    'nullable' => false,
+                    'primary' => true,
+                ],
+                'Store Id'
+            )
+            ->addIndex(
+                $installer->getIdxName('lof_category_banner_store', ['banner_id']),
+                ['banner_id']
+            )->addForeignKey(
+                $installer->getFkName('lof_category_banner_store', 'banner_id', 'lof_category_banner', 'banner_id'),
+                'banner_id',
+                $installer->getTable('lof_category_banner'),
+                'banner_id',
+                \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
+            )->addForeignKey(
+                $installer->getFkName('lof_category_banner_store', 'store_id', 'store', 'store_id'),
+                'store_id',
+                $installer->getTable('store'),
+                'store_id',
+                \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
+            );
 
         $lof_category_banner_category_table = $installer->getConnection()->newTable($installer->getTable('lof_category_banner_category'))
-                ->addColumn(
-                    'banner_id',
-                    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
-                    null,
-                    ['unsigned' => true, 'nullable' => false, 'primary' => true],
-                    'Banner ID'
-                )
-                ->addColumn(
-                    'category_id',
-                    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
-                    null,
-                    [
-                     'unsigned' => true,
-                     'nullable' => false
-                    ],
-                    'Catalog Category Id'
-                )
-                ->addIndex(
-                    $installer->getIdxName(('lof_category_banner_category', ['banner_id']),
-                    ['banner_id']
-                )->addForeignKey(
-                    $installer->getFkName($'lof_category_banner_category', 'banner_id', 'lof_category_banner', 'banner_id'),
-                    'banner_id',
-                    $installer->getTable('lof_category_banner'),
-                    'banner_id',
-                    \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
-                );
-                
+            ->addColumn(
+                'banner_id',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['unsigned' => true, 'nullable' => false, 'primary' => true],
+                'Banner ID'
+            )
+            ->addColumn(
+                'category_id',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                [
+                    'unsigned' => true,
+                    'nullable' => false
+                ],
+                'Catalog Category Id'
+            )
+            ->addIndex(
+                $installer->getIdxName('lof_category_banner_category', ['banner_id']),
+                ['banner_id']
+            )->addForeignKey(
+                $installer->getFkName('lof_category_banner_category', 'banner_id', 'lof_category_banner', 'banner_id'),
+                'banner_id',
+                $installer->getTable('lof_category_banner'),
+                'banner_id',
+                \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
+            );
+
         $installer->getConnection()->createTable($lof_category_banner_table);
         $installer->getConnection()->createTable($lof_category_banner_store_table);
         $installer->getConnection()->createTable($lof_category_banner_category_table);
-                
+
         $installer->endSetup();
     }
 }
