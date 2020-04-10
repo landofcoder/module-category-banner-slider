c32d6296a6ef5f377733949d626b54dc305eb2a0
hieu <hieu01412@gmail.com>
2020-04-10 17:45:35 +0700

    push code

--- Controller/Adminhtml/CategoryBanner/Upload.php
+++ Controller/Adminhtml/CategoryBanner/Upload.php
@@ -1,63 +1,131 @@
 <?php
 /**
- * Upload
- *
- * @copyright Copyright © 2020 landofcoder. All rights reserved.
- * @author    landofcoder@gmail.com
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
  */
-
 namespace Lof\CategoryBannerSlider\Controller\Adminhtml\CategoryBanner;
 
-use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\App\Action\HttpPostActionInterface as HttpPostActionInterface;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Exception\LocalizedException;
 
 /**
  * Class Upload
  */
-class Upload extends \Magento\Backend\App\Action
+class Upload extends \Magento\Backend\App\Action implements HttpPostActionInterface
 {
     /**
-     * @var \Lof\CategoryBannerSlider\Model\ImageUploader
+     * Authorization level of a basic admin session
+     *
+     * @see _isAllowed()
      */
-    public $imageUploader;
+    const ADMIN_RESOURCE = 'Lof_CategoryBannerSlider::CategoryBanner';
 
     /**
-     * Upload constructor.
+     * @var \Magento\Framework\Controller\Result\RawFactory
+     */
+    protected $resultRawFactory;
+
+    /**
+     * @var array
+     */
+    private $allowedMimeTypes = [
+        'jpg' => 'image/jpg',
+        'jpeg' => 'image/jpeg',
+        'gif' => 'image/png',
+        'png' => 'image/gif'
+    ];
+
+    /**
+     * @var \Magento\Framework\Image\AdapterFactory
+     */
+    private $adapterFactory;
+
+    /**
+     * @var \Magento\Framework\Filesystem
+     */
+    private $filesystem;
+
+    /**
+     * @var \Magento\Catalog\Model\Product\Media\Config
+     */
+    private $productMediaConfig;
+
+    /**
      * @param \Magento\Backend\App\Action\Context $context
-     * @param \Lof\CategoryBannerSlider\Model\ImageUploader $imageUploader
+     * @param \Magento\Framework\Controller\Result\RawFactory $resultRawFactory
+     * @param \Magento\Framework\Image\AdapterFactory $adapterFactory
+     * @param \Magento\Framework\Filesystem $filesystem
+     * @param \Magento\Catalog\Model\Product\Media\Config $productMediaConfig
      */
     public function __construct(
         \Magento\Backend\App\Action\Context $context,
-        \Lof\CategoryBannerSlider\Model\ImageUploader $imageUploader
+        \Magento\Framework\Controller\Result\RawFactory $resultRawFactory,
+        \Magento\Framework\Image\AdapterFactory $adapterFactory = null,
+        \Magento\Framework\Filesystem $filesystem = null,
+        \Magento\Catalog\Model\Product\Media\Config $productMediaConfig = null
     ) {
         parent::__construct($context);
-        $this->imageUploader = $imageUploader;
+        $this->resultRawFactory = $resultRawFactory;
+        $this->adapterFactory = $adapterFactory ?: ObjectManager::getInstance()
+            ->get(\Magento\Framework\Image\AdapterFactory::class);
+        $this->filesystem = $filesystem ?: ObjectManager::getInstance()
+            ->get(\Magento\Framework\Filesystem::class);
+        $this->productMediaConfig = $productMediaConfig ?: ObjectManager::getInstance()
+            ->get(\Magento\Catalog\Model\Product\Media\Config::class);
     }
 
     /**
-     * @return bool
-     */
-    public function _isAllowed()
-    {
-        return $this->_authorization->isAllowed('Lof_CategoryBannerSlider::CategoryBanner');
-    }
-
-    /**
-     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface
+     * Upload image(s) to the product gallery.
+     *
+     * @return \Magento\Framework\Controller\Result\Raw
      */
     public function execute()
     {
         try {
-            $result = $this->imageUploader->saveFileToTmpDir('images');
-            $result['cookie'] = [
-                'name' => $this->_getSession()->getName(),
-                'value' => $this->_getSession()->getSessionId(),
-                'lifetime' => $this->_getSession()->getCookieLifetime(),
-                'path' => $this->_getSession()->getCookiePath(),
-                'domain' => $this->_getSession()->getCookieDomain(),
-            ];
+            $uploader = $this->_objectManager->create(
+                \Magento\MediaStorage\Model\File\Uploader::class,
+                ['fileId' => 'image']
+            );
+            $uploader->setAllowedExtensions($this->getAllowedExtensions());
+            $imageAdapter = $this->adapterFactory->create();
+            $uploader->addValidateCallback('catalog_product_image', $imageAdapter, 'validateUploadFile');
+            $uploader->setAllowRenameFiles(true);
+            $uploader->setFilesDispersion(true);
+            $mediaDirectory = $this->filesystem->getDirectoryRead(DirectoryList::MEDIA);
+            $result = $uploader->save(
+                $mediaDirectory->getAbsolutePath($this->productMediaConfig->getBaseTmpMediaPath())
+            );
+
+            $this->_eventManager->dispatch(
+                'catalog_product_gallery_upload_image_after',
+                ['result' => $result, 'action' => $this]
+            );
+
+            unset($result['tmp_name']);
+            unset($result['path']);
+
+            $result['url'] = $this->productMediaConfig->getTmpMediaUrl($result['file']);
+            $result['file'] = $result['file'] . '.tmp';
         } catch (\Exception $e) {
             $result = ['error' => $e->getMessage(), 'errorcode' => $e->getCode()];
         }
-        return $this->resultFactory->create(ResultFactory::TYPE_JSON)->setData($result);
+
+        /** @var \Magento\Framework\Controller\Result\Raw $response */
+        $response = $this->resultRawFactory->create();
+        $response->setHeader('Content-type', 'text/plain');
+        $response->setContents(json_encode($result));
+        return $response;
+    }
+
+    /**
+     * Get the set of allowed file extensions.
+     *
+     * @return array
+     */
+    private function getAllowedExtensions()
+    {
+        return array_keys($this->allowedMimeTypes);
     }
 }
